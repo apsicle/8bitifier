0cf942dae011bb48744642bd77af7322ed0e1690
apsicle <rcyan@email.wm.edu>
2016-11-20 20:57:17 -0500

    coloring improvements and examples

--- eightbitify.py
+++ eightbitify.py
@@ -5,146 +5,123 @@ import bisect
 import nes
 
 #sample usage ---- x = imread("food.jpg") => x = ndarray(height(x), width(y)), 256 val RGB elements
+def go(speed = np.e, img = "cm.jpg", order = "01"):
+    '''ordering: 0 = recolor, 1 = hue.'''
 
-nes_file = file("nes_palette.txt")
-text = nes_file.readlines()
-data = [line.rstrip('\n') for line in text]
-data = [line.split(",") for line in data]
-data = [map(int, line) for line in data]
-
-keys = np.array(data)
-distance = np.sqrt(keys * keys)
-distance = [sum(pixelval) for pixelval in distance]
-X = data
-Y = map(int, distance)
-
-data_by_distance = [x for (y,x) in sorted(zip(Y,X))]
-distance = sorted(Y)
+    array = imread(img).astype(int)
+    recolor(array)
+    hue(array, speed)
 
 
+    #160 across is really nice (ie. xdim / rate = 160.
+    outstring = img.split(".")
+    outstring = outstring[0] + "hue%d"%speed + "recolorauto" + ".jpg"
+    imsave(outstring, array)
 
-def bitify(filename, rate = 10, method = "recolor"):
-    array = imread(filename)
-    if method == "recolor":
-        recolored = recolor(array, rate)
-    elif method == "colorize":
-        recolored = colorize(array)
-    name = filename.split(".")
-    outstring = name[0] + method + "." + name[1]
-    imsave(outstring, recolored)
+def sig(x, speed = np.e):
+    return 256/(1+speed**(-(x-128)/32.0))
 
-palette = list(range(0,257,  8))
-
-nes_palette = data_by_distance
-palette = [list(range(0,257,  8)), list(range(0,257,  8)), list(range(0,257,  8))]
-palette1 = []
-palette1.append([x[0] for x in nes_palette])
-palette1.append([x[1] for x in nes_palette])
-palette1.append([x[2] for x in nes_palette])
+def sigmoid(x):
+    return 1/(1+np.exp(-x))
 
 #distance is imported
+def hue(array, speed):
+    palette = [list(range(0,257,  32)), list(range(0,257,  32)), list(range(0,257,  32))]
+    palette[2][0] = 1
+    xdim = array.shape[1]
+    ydim = array.shape[0]
+    count = 0
+    for i in np.arange(ydim):
+        for j in np.arange(xdim):
 
-def colorize(array, method = "range", palette1 = palette):
-    if method == "range":
-        xdim = array.shape[1]
-        ydim = array.shape[0]
-        count = 0
-        for i in np.arange(ydim):
-            for j in np.arange(xdim):
-                print count
-                count += 1
-                r = array[i][j][0]
-                g = array[i][j][1]
-                b = array[i][j][2]
+            count += 1
+            r = sig(array[i][j][0], speed)
+            g = sig(array[i][j][1], speed)
+            b = sig(array[i][j][2], speed)
+            if i == 85 and j == 114:
+                if(r >= palette[0][-1]):
+                    pass
+                else:
+                    r_1 = bisect.bisect_right(palette[0], r)
+                    array[i][j][0] = max(palette[0][r_1], palette[0][r_1-1])
 
-                #This section rounds the RGB elements to the nearest color in 'palette'
-                if(r >= palette1[0][-1]):
+                if(g >= palette[1][-1]):
+                    pass
+                else:
+                    g_1 = bisect.bisect_right(palette[1], g)
+                    array[i][j][1] = max(palette[1][g_1], palette[1][g_1-1])
+
+                if(b >= palette[2][-1]):
                    pass
                 else:
-                    r_1 = bisect.bisect_right(palette1[0], r)
-                    array[i][j][0] = max(palette1[0][r_1], palette1[0][r_1-1])
+                    b_1 = bisect.bisect_right(palette[2], b)
+                    x = max(palette[2][b_1], palette[2][b_1-1])
+                    print "%d <-this is to be put into array"%x
+                    print "%d <- this is the current array value"%array[i][j][2]
+                    array[i][j][2] = x #assignment
+                    print "%d <- this is array after assignment"%array[i][j][2]
+            else:
+                if(r >= palette[0][-1]):
+                    pass
+                else:
+                    r_1 = bisect.bisect_right(palette[0], r)
+                    array[i][j][0] = max(palette[0][r_1], palette[0][r_1-1])
 
-                if(g >= palette1[1][-1]):
+                if(g >= palette[1][-1]):
+                    pass
+                else:
+                    g_1 = bisect.bisect_right(palette[1], g)
+                    array[i][j][1] = max(palette[1][g_1], palette[1][g_1-1])
+
+                if(b >= palette[2][-1]):
                    pass
                 else:
-                    g_1 = bisect.bisect_right(palette1[1], g)
-                    array[i][j][1] = max(palette1[1][g_1], palette1[1][g_1-1])
-
-                if(b >= palette1[2][-1]):
-                   pass
-                else:
-                    print i,j
-
-                    b_1 = bisect.bisect_right(palette1[2], b)
-                    print b_1
-                    array[i][j][2] = max(palette1[2][b_1], palette1[2][b_1-1])
-        return array
-    if method == "nes":
-        count = 0
-        xdim = array.shape[1]
-        ydim = array.shape[0]
-        for i in np.arange(ydim):
-            for j in np.arange(xdim):
-                print count
-                count += 1
-
-                pixel_distance = np.sqrt(array[i][j][0]**2 + array[i][j][1]**2 + array[i][j][2]**2)
-                if pixel_distance >= distance[-1]:
-                    array[i][j] = nes_palette[-1]
-                else:
-                    tester = bisect.bisect_right(distance, pixel_distance)
-                    if distance[tester] - pixel_distance > pixel_distance - distance[tester-1]:
-                        array[i][j] = nes_palette[tester]
-                    else:
-                        array[i][j] = nes_palette[tester-1]
-        return array
+                    b_1 = bisect.bisect_right(palette[2], b)
+                    array[i][j][2] = max(palette[2][b_1], palette[2][b_1 - 1])
 
 
-def avgBlock(x,y, array, size):
+            #Want a function that is zero-centered with positive slope >1 increasing with larger magnitude
+
+def avgBlock(y,x, array, ratex, ratey):
     #assuming block of g h i
     #                  d e f
     #                  a b c
     #where x, y are the coordinates of pixel a, avgBlock(0,0, array, size = 3) will
     #produce a pixel with the averages of the 3x3 pixel shown above.
     avg_pixel = [0,0,0]
-    for i in np.arange(size):
-        for j in np.arange(size):
-            a = array[x + i][y + j]
+    for i in np.arange(ratey):#y
+        for j in np.arange(ratex):
+            a = array[y + i][x + j]
             avg_pixel += a
-    avg_pixel = avg_pixel / (size*size)
+    avg_pixel = avg_pixel / (ratey*ratex)
     return avg_pixel.astype(int)
 
-def fillBlock(x,y, array_to_fill, pixel_to_use, size):
-    for i in np.arange(size):
-        for j in np.arange(size):
-            array_to_fill[x+i][y+j] = pixel_to_use
+def fillBlock(y,x, array_to_fill, pixel_to_use, ratex, ratey):
+    for i in np.arange(ratey):
+        for j in np.arange(ratex):
+            array_to_fill[y+i][x+j] = pixel_to_use
 
-def recolor(array, rate):
+def recolor(array):
+    #This function is not inplace ie. doesn't modify the original array
     xdim = array.shape[1]
+    ratex = xdim / 160
+    if((ratex * 160) - xdim > ((ratex+1) * 160) - xdim):
+        ratex = ratex + 1
     ydim = array.shape[0]
-    for i in np.arange(ydim / rate):
-        for j in np.arange(xdim / rate):
-            pixel = avgBlock(i*rate, j*rate, array, rate)
-            fillBlock(i*rate, j*rate, array, pixel, rate)
+    ratey = ydim / 128
+    if((ratey * 128) - ydim > ((ratey+1) * 160) - ydim):
+        ratey = ratey + 1
+    i = 0
+    while((i+1) * ratey < ydim):
+        j = 0
+        print i, j, ratex, ratey, xdim, ydim
+        while((j+1) * ratex < xdim):
+            print i, j, ratex, ratey, xdim, ydim
+            pixel = avgBlock(i*ratey, j*ratex, array, ratex, ratey)
+            fillBlock(i*ratey, j*ratex, array, pixel, ratex, ratey)
+            j += 1
+        i += 1
     return array
-
-def saturate(array, exponent):
-    xdim = array.shape[1]
-    ydim = array.shape[0]
-    for i in np.arange(ydim):
-        for j in np.arange(xdim):
-            array[i][j][0] = array[i][j][0]**exponent
-            array[i][j][1] = array[i][j][1]**exponent
-            array[i][j][2] = array[i][j][2]**exponent
-    return array
-
-cm = imread("cm.jpg")
-def reset(array = cm):
-    cm = imread("cm.jpg")
-
-
-recolored = recolor("food.jpg", 20)
-imsave("testrecolor.png", recolored)
 
 def compress(array,rate):
     xdim = array.shape[1]
@@ -154,9 +131,6 @@ def compress(array,rate):
         for j in np.arange(xdim / rate):
             compressed[i][j] = avgBlock(i*rate,j*rate,array,rate)
     return compressed
-
-compressed = compress(food, 5)
-imsave("testcompressed.png", compressed)
 
 
 
--- nes.py
+++ nes.py
@@ -15,3 +15,109 @@ Y = distance
 data_by_distance = [x for (y,x) in sorted(zip(Y,X))]
 distance = sorted(distance)
 
+# Belongs in eightbitify.py but is deprecated
+
+# def colorize(array, method = "range", palette1 = palette):
+#     if method == "range":
+#         xdim = array.shape[1]
+#         ydim = array.shape[0]
+#         count = 0
+#         for i in np.arange(ydim):
+#             for j in np.arange(xdim):
+#                 print count
+#                 count += 1
+#                 r = array[i][j][0]
+#                 g = array[i][j][1]
+#                 b = array[i][j][2]
+#
+#                 if(r >= palette1[0][-1]):
+#                    pass
+#                 else:
+#                     r_1 = bisect.bisect_right(palette1[0], r)
+#                     array[i][j][0] = max(palette1[0][r_1], palette1[0][r_1-1])
+#
+#                 if(g >= palette1[1][-1]):
+#                    pass
+#                 else:
+#                     g_1 = bisect.bisect_right(palette1[1], g)
+#                     array[i][j][1] = max(palette1[1][g_1], palette1[1][g_1-1])
+#
+#                 if(b >= palette1[2][-1]):
+#                    pass
+#                 else:
+#                     print i,j
+#
+#                     b_1 = bisect.bisect_right(palette1[2], b)
+#                     print b_1
+#                     array[i][j][2] = max(palette1[2][b_1], palette1[2][b_1-1])
+#         return array
+#                 #This section rounds the RGB elements to the nearest color in 'palette'
+#                 if(r >= palette1[0][-1]):
+#                    pass
+#                 else:
+#                     r_1 = bisect.bisect_right(palette1[0], r)
+#                     array[i][j][0] = max(palette1[0][r_1], palette1[0][r_1-1])
+#
+#                 if(g >= palette1[1][-1]):
+#                    pass
+#                 else:
+#                     g_1 = bisect.bisect_right(palette1[1], g)
+#                     array[i][j][1] = max(palette1[1][g_1], palette1[1][g_1-1])
+#
+#                 if(b >= palette1[2][-1]):
+#                    pass
+#                 else:
+#                     print i,j
+#
+#                     b_1 = bisect.bisect_right(palette1[2], b)
+#                     print b_1
+#                     array[i][j][2] = max(palette1[2][b_1], palette1[2][b_1-1])
+#         return array
+#     if method == "nes":
+#         count = 0
+#         xdim = array.shape[1]
+#         ydim = array.shape[0]
+#         for i in np.arange(ydim):
+#             for j in np.arange(xdim):
+#                 print count
+#                 count += 1
+#
+#                 pixel_distance = np.sqrt(array[i][j][0]**2 + array[i][j][1]**2 + array[i][j][2]**2)
+#                 if pixel_distance >= distance[-1]:
+#                     array[i][j] = nes_palette[-1]
+#                 else:
+#                     tester = bisect.bisect_right(distance, pixel_distance)
+#                     if distance[tester] - pixel_distance > pixel_distance - distance[tester-1]:
+#                         array[i][j] = nes_palette[tester]
+#                     else:
+#                         array[i][j] = nes_palette[tester-1]
+#         return array
+
+# Deprecated
+#
+# nes_file = file("nes_palette.txt")
+# text = nes_file.readlines()
+# data = [line.rstrip('\n') for line in text]
+# data = [line.split(",") for line in data]
+# data = [map(int, line) for line in data]
+#
+# keys = np.array(data)
+# distance = np.sqrt(keys * keys)
+# distance = [sum(pixelval) for pixelval in distance]
+# X = data
+# Y = map(int, distance)
+#
+# data_by_distance = [x for (y,x) in sorted(zip(Y,X))]
+# distance = sorted(Y)
+#
+#
+#
+# def bitify(filename, rate = 10, method = "recolor"):
+#     array = imread(filename)
+#     if method == "recolor":
+#         recolored = recolor(array, rate)
+#     elif method == "colorize":
+#         recolored = colorize(array)
+#     name = filename.split(".")
+#     outstring = name[0] + method + "." + name[1]
+#     imsave(outstring, recolored)
